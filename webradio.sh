56c6b90374fd4377276dbafa9221fa3a3053ef05
Loic Dubois <loic_dubois@live.fr>
2020-07-29 21:25:53 +0200

    Initial startup script for the webradio app

---
+++ webradio.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Turn off bluetooth on startup (I am using the BT receiver of the amp)
+rfkill block bluetooth
+# Uncomment below if the above doesn't work with a Lenovo Thinkpad (I haven't tested it myself)
+# TODO: add source
+#echo disable > /proc/acpi/ibm/bluetooth
+
+# Start webradio app
+odio 
+# Make it fullscreen
+wmctrl -r odio -e 0,0,0,1920,1200
