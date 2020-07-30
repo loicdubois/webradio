15d3b309c976488bff2e27b0e1f605bd047a8989
Loïc Dubois <loic_dubois@live.fr>
2020-07-30 11:09:28 +0200

    Remove hiding the docking bar from setup script

--- setup.sh
+++ setup.sh
@@ -4,8 +4,3 @@
 sudo apt update
 sudo apt install wmctrl snapd -y
 sudo snap install odio
-
-# Hide the docking bar
-gsettings set org.gnome.shell.extensions.dash-to-dock autohide false
-gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
-gsettings set org.gnome.shell.extensions.dash-to-dock intellihide false
